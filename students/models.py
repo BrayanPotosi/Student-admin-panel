27432968366e83324fea971c1673f8a9c155368e
Brayan Potosi <bp990308@hotmail.com>
2021-06-04 15:46:20 -0500

    [UPDATE] .gitignore file : .idea folder was excluded

--- students/models.py
+++ students/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class Vertical(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class Cohort(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -12,11 +14,13 @@ class Cohort(models.Model):
     def __str__(self):
         return self.name
 
+
 class Team(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
