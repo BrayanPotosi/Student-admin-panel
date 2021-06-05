48e59e6ea2c4b27d8ddaf8a3f6c04f5a907ffc6f
Brayan Potosi <bp990308@hotmail.com>
2021-06-05 11:40:18 -0500

    [ADD] Path from students views in: urls file

--- students/views.py
+++ students/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from students.models import Student
 
 # Create your views here.
 
+
+def get_students(request):
+    list_students = Student.objects.get(id=1)
+    return render(request, template_name='students.html')
