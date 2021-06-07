8b8a48db21a53cb4903651d7c8b3b9b0cf3ee929
Brayan Potosi <bp990308@hotmail.com>
2021-06-07 14:02:48 -0500

    [ADD] delete student view

--- students/views.py
+++ students/views.py
@@ -1,49 +1,63 @@
 from django.shortcuts import render, redirect
-from students.models import Student, Cohort, Vertical, Team
-import random
+from students.models import Student
+from students.forms import FormStudent
+from random import randint
 # Create your views here.
 
 
 def list_students(request):
     students_list = Student.objects.all()
-    vertical_list = Vertical.objects.all()
-    cohorts_list = Cohort.objects.all()
-    team_list = Team.objects.all()
+    form_student = FormStudent()
 
     return render(request, template_name='students.html',
                   context={
                       'students_list': students_list,
-                      'vertical_list': vertical_list,
-                      'cohorts_list': cohorts_list,
-                      'team_list': team_list
+                      'form': form_student
                   })
 
 
 def create_student(request):
 
     if request.method == 'POST':
-        first_name = request.POST['fname']
-        last_name = request.POST['lname']
-        dni = request.POST['fdni']
-        username = f'{first_name[:3]}{last_name[:3]}{random.randint(100, 9999)}'
-        vertical = request.POST['fvertical']
-        cohort = request.POST['fcohort']
-        team = request.POST['fteam']
 
-        vertical_id = Vertical.objects.get(name__exact=vertical)
-        cohort_id = Cohort.objects.get(name__exact=cohort)
-        team_id = Team.objects.get(name__exact=team)
+        student_form = FormStudent(request.POST)
 
-        student = Student(
-            first_name=first_name,
-            last_name=last_name,
-            dni=dni,
-            username= username,
-            vertical_id=vertical_id.id,
-            cohort_id=cohort_id.id,
-            team_id=team_id.id
-        )
+        if student_form.is_valid():
+            data_form = student_form.cleaned_data
 
-        student.save()
+            print(request.POST)
 
-        return redirect('students')
+            first_name = data_form['first_name']
+            last_name = data_form['last_name']
+            dni = data_form['dni']
+            username = f'{first_name[:3]}{last_name[:3]}{randint(1000, 9999)}'
+            vertical = request.POST.get('vertical')
+            cohort = request.POST.get('cohort')
+            team = request.POST.get('team')
+
+            student = Student(
+                first_name=first_name,
+                last_name=last_name,
+                dni=dni,
+                username=username,
+                vertical_id=vertical,
+                cohort_id=cohort,
+                team_id=team
+            )
+
+            student.save()
+
+            return redirect('students')
+
+
+def delete_student(request):
+
+    if request.method == 'POST':
+
+        student_id = request.POST.get('deleteb')
+        student = Student.objects.get(pk=student_id)
+
+        student.delete()
+
+    return redirect('students')
+
