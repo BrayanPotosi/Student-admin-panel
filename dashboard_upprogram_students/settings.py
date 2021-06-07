cbe22e2a90fcd679aff9ed5fe26c03dd38cb680a
Wilson Antury <ventas@antury.net>
2021-06-07 16:33:55 -0500

    [UPDATE] collect statics

--- dashboard_upprogram_students/settings.py
+++ dashboard_upprogram_students/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 
@@ -149,6 +149,7 @@ LOGIN_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
