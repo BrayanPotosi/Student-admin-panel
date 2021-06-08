4a56096e8af406477957d161c708ed3f2f894b1d
Wilson Antury <ventas@antury.net>
2021-06-07 21:19:55 -0500

    [UPDATE] social buttons styles

--- dashboard_upprogram_students/settings.py
+++ dashboard_upprogram_students/settings.py
@@ -100,23 +100,23 @@ WSGI_APPLICATION = 'dashboard_upprogram_students.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env("DATABASE"),
-#         'USER': env("DB_USER"),
-#         'PASSWORD': ("DB_PASSWORD"),
-#         'HOST': 'db',
-#         'PORT': ''
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DATABASE"),
+        'USER': env("DB_USER"),
+        'PASSWORD': ("DB_PASSWORD"),
+        'HOST': 'db',
+        'PORT': ''
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
